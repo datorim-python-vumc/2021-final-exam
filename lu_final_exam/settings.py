f408c0781ad3cda27114721034d89f9f5baf94bf
Maksims Piļščikovs <m.pilscikov@gmail.com>
2021-05-02 22:34:03 +0300

    Set  DEGUG = False

--- lu_final_exam/settings.py
+++ lu_final_exam/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$w=x=hel8fr@s&=knb0qp=ikasu^exvk6-*1n1jq6v+jc02@s$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
